cb18380c12b7cd8a008b60cac972258d88783da0
Config Server <config@example.com>
2025-08-07 16:18:27 +0300

    модифицировал JsonFileReader и добавил README.md

--- src/main/java/by/koval/JsonFileReader.java
+++ src/main/java/by/koval/JsonFileReader.java
@@ -2,12 +2,15 @@ package by.koval;
 
 import com.google.gson.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class JsonFileReader {
     public List<Ticket> getTicketsFromJson (String fileName) {
@@ -16,16 +19,29 @@ public class JsonFileReader {
                 .registerTypeAdapter(LocalDate.class, new CustomDateTimeAdapter())
                 .registerTypeAdapter(LocalTime.class, new CustomDateTimeAdapter())
                 .create();
+        boolean fileFound = false;
 
-        try (FileReader reader = new FileReader(fileName)) {
-            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
-            JsonElement ticketsElement = jsonObject.get("tickets");
-            for (JsonElement ticketElement : ticketsElement.getAsJsonArray()) {
-                tickets.add(gson.fromJson(ticketElement, Ticket.class));
+        while (!fileFound) {
+            try (FileReader reader = new FileReader(fileName)) {
+                fileFound = true;
+                JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+                JsonElement ticketsElement = jsonObject.get("tickets");
+                for (JsonElement ticketElement : ticketsElement.getAsJsonArray()) {
+                    tickets.add(gson.fromJson(ticketElement, Ticket.class));
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл tickets.json не найден: " + fileName
+                        + ". Убедитесь, что файл находится в папке resources и нажмите Enter или укажите путь к файлу вручную: ");
+                String input = new Scanner(System.in).nextLine();
+                if (new File(input).exists()) {
+                    fileName = input;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
         return tickets;
+
     }
 }
--- src/main/java/by/koval/Main.java
+++ src/main/java/by/koval/Main.java
@@ -4,18 +4,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
         int div = 120;
-        List<Ticket> tickets = new JsonFileReader().getTicketsFromJson("src/main/resources/tickets.json");
+        List<Ticket> tickets = new JsonFileReader()
+                .getTicketsFromJson("C:\\Users\\kas\\AirTickets\\src\\main\\resources\\tickets.json");
 
         List<Ticket> filteredTickets = Calculator.getTicketsFilteredOriginDest(tickets, "VVO", "TLV");
         List<Ticket> minPriceTicketsForEachCarrier = Calculator.getTicketsWithMinTimeOfEachCarrier(filteredTickets);
         Double averagePrice = Calculator.getAveragePrice(filteredTickets);
         Double medianPrice = Calculator.getMedianPrice(filteredTickets);
-
-        for (Ticket ticket : filteredTickets) {
-            System.out.println(ticket);
-            System.out.println(Calculator.getDuration(ticket).toHours() + " ч " + Calculator.getDuration(ticket).toMinutes() % 60 + " мин");
-        }
 
         System.out.println("-".repeat(div));
 
